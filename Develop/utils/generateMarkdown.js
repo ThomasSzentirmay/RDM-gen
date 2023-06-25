480718d3586fddaee8547b2e3c6ac9ead667716a
Thomas Szentirmay <tlszent@outlook.com>
2023-06-25 13:52:04 -0400

    created function to return strings of messages depending on users choice of licenese or no license

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -18,7 +18,13 @@ function renderLicenseBadge(license) {
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'N/A') {
+    return 'This project does not have a specific license.';
+  } else {
+    return `This project is licensed under the ${license}.`;
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
