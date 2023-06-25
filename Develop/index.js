cb322b975e998f4c7abe8eb062da0163e2b81ecb
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-06-24 21:48:26 -0400

    create an array of questions prmopted to users

--- Develop/index.js
+++ Develop/index.js
@@ -3,16 +3,52 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 // Create an array of questions for user input
-const questions = [];
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Please enter project title:',
+        name: 'title'
+    },
+    {
+        type: 'input',
+        message: 'Please enter project description:',
+        name: 'description'
+    },
+    {
+        type: 'input',
+        message: 'Please enter project installtion instructions:',
+        name: 'installation'
+    },
+    {
+        type: 'input',
+        message: 'Please enter project usage info:',
+        name: 'usage'
+    },
+    {
+        type: 'input',
+        message: 'Please enter contribution guidelines:',
+        name: 'contribution'
+    },
+    {
+        type: 'input',
+        message: 'Please enter test intructions:',
+        name: 'test'
+    },
+    {
+        type: 'list',
+        message: 'Please choose a project license:',
+        name: 'license',
+        choices: ['MIT license', 'Apache License 2.0', 'GNU v3.0', 'BSD 2-Clause Simplified License', 'Boost Software License 1.0', 'N/A']
 
-
-// Create a function to write README file
-function writeToFile(fileName, data) {}
-
-
-// Create a function to initialize app
-function init() {}
-
-
-// Function call to initialize app
-init();
+    },
+    {
+        type: 'input',
+        message: 'Please enter your GitHub username:',
+        name: 'username'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email',
+        name: 'email'
+    }
+]);
