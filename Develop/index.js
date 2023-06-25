fa43e77be48e3a29e0745d1df85331a252f39dd1
ThomasSzentirmay <132217664+ThomasSzentirmay@users.noreply.github.com>
2023-06-24 22:06:42 -0400

    created function to write data to a new md file

--- Develop/index.js
+++ Develop/index.js
@@ -99,3 +99,13 @@ inquirer.prompt([
     * 
     `
 });
+
+// Function to write new file
+function createNewFile (fileName, data) {
+    fs.writeFile(`./${fileName.toLowerCase().split(' ').join('')}.md`, data, (err) => {
+        if(err){
+            console.log(err);
+        }
+        console.log('Your README file has been generated')
+    });
+};
